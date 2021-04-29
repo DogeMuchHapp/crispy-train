1be973a9d08b007851264bc173786df7e6381398
DogeMuchHapp <83430363+DogeMuchHapp@users.noreply.github.com>
2021-04-29 17:15:33 -0400

    Add files via upload

---
+++ xDD.py
@@ -0,0 +1,21 @@
+# Ur dumb ok, stop looking into other peoples junk, it's weird, pls stop
+# Just download python, its a cool program, c# is dumb, don't run this in c# cause it'll act weird lol
+# Imagine reading this in like notepad or something, kinda weird ngl
+# Also just in case this makes it to CNN um, DMH!
+num1 = float(input("Please input a number:"))
+function = input("Please input a function (Add, Subtract, Multiply, Divide, Square, Cube):")
+num2 = float(input("Please input a second number:"))
+if function == "Add":
+    print(num1 + num2)
+elif function == "Subtract":
+    print(num1 - num2)
+elif function == "Multiply":
+    print(num1 * num2)
+elif function == "Divide":
+    print(num1 / num2)
+elif function == "Square":
+    print(num1 * num1)
+elif function == "Cube":
+    print(num1 * num1 * num1)
+continue1 = input("Are you satisfied?")
+print("You just got pranked!")
